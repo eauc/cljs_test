b4e692355ba8595ed87e4d68b61b9d8fdfbeaff4
Auclair Emmanuel <auclair.emmanuel.prestataire@sfr.com>
2017-04-10 11:49:37 +0200

    Nodejs

--- src/hello_world/core.cljs
+++ src/hello_world/core.cljs
@@ -1,13 +1,18 @@
 ;; (ns hello-world.core)
+;; (ns hello-world.core
+;;	 (:require [clojure.browser.repl :as repl]))
 (ns hello-world.core
-  (:require [clojure.browser.repl :as repl]))
+	(:require [cljs.nodejs :as nodejs]))
 
-(defonce conn
-  (repl/connect "http://localhost:9000/repl"))
+;; (defonce conn
+;; 	(repl/connect "http://localhost:9000/repl"))
 
-(enable-console-print!)
+(nodejs/enable-util-print!)
 
-(println "Hello, bitch!")
+(defn -main [& args]
+	(println "Hello, bitch!"))
+
+(set! *main-cli-fn* -main)
 
 (defn foo [a b]
-  (* a b))
+	(* a b))
